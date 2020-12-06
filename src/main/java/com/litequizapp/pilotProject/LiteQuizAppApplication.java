73a7edbbc652980101decb5d9f398b0f32c0a3d0
Sergei <53571364+sshitov@users.noreply.github.com>
2020-12-06 19:52:53 +0400

    - changed project on downloaded from Spring Initializr - added basic description of the app

---
+++ src/main/java/com/litequizapp/pilotProject/LiteQuizAppApplication.java
@@ -0,0 +1,15 @@
+package com.litequizapp.pilotProject;
+
+import java.util.Arrays;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteQuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteQuizAppApplication.class, args);
+		System.out.println("test");
+	}
+
+}
---
+++ src/test/java/com/litequizapp/pilotProject/LiteQuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litequizapp.pilotProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteQuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
